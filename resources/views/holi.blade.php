12090928f705fd3fb087675e0b5dfb881303c86e
unknown <xiomara@gmail.com>
2019-09-26 23:29:04 -0500

    proyecto base

---
+++ resources/views/holi.blade.php
@@ -0,0 +1,20 @@
+ 
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        <button type="button" class="btn btn-primary">Vidrios Templados</button>
+<button type="button" class="btn btn-secondary">Socalos</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+        </div>
+    </div>
+</div>
+@endsection
